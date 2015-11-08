a3637a11127305533dd1b077933f455f7a58b021
adblancod <adblancod@gmail.com>
2015-11-08 11:56:15 +0000

    Added Sdk Firebase

--- app/src/main/java/gdg/spanish/themoviedb/MainActivity.java
+++ app/src/main/java/gdg/spanish/themoviedb/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
 
 
