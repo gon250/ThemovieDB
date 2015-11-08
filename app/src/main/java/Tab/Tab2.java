09aaa68222c8440a51b57fe600e6e409870c8ba0
Gonzalo <zalo250@gmail.com>
2015-11-08 11:30:47 +0000

    Adding tabs

---
+++ app/src/main/java/Tab/Tab2.java
@@ -0,0 +1,24 @@
+package Tab;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gdg.spanish.themoviedb.R;
+
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_2, null);
+
+        //TODO: Implement content
+
+        return v;
+    }
+
+}
--- app/src/main/java/gdg/spanish/themoviedb/MainActivity.java
+++ app/src/main/java/gdg/spanish/themoviedb/MainActivity.java
@@ -1,15 +1,66 @@
 package gdg.spanish.themoviedb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import Adapters.TabAdapter;
+import Tab.Tab1;
+import Tab.Tab2;
+import Tab.Tab3;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Toolbar & tabs
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabs;
+    private TabAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+
+        //TODO: Manage Error
+        // Toolbar
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1(), this.getResources().getString(R.string.text_tab_title_1));
+        adapter.addFragment(new Tab2(), this.getResources().getString(R.string.text_tab_title_2));
+        adapter.addFragment(new Tab3(), this.getResources().getString(R.string.text_tab_title_3));
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 }
