6e668fa4ef65f91d085d8b0a054ef56497aec6ea
Gonzalo <zalo250@gmail.com>
2015-11-08 15:15:00 +0000

    Adding Floating button

--- app/src/main/java/gdg/spanish/themoviedb/MainActivity.java
+++ app/src/main/java/gdg/spanish/themoviedb/MainActivity.java
@@ -1,12 +1,15 @@
 package gdg.spanish.themoviedb;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.firebase.client.Firebase;
 
@@ -40,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
         // Set Tabs inside Toolbar
         tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     private void setupViewPager(ViewPager viewPager) {
