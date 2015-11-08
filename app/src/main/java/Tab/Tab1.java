9680f33d385c2092fcaf44d5d782bc710d4551d7
Gonzalo <zalo250@gmail.com>
2015-11-08 13:55:50 +0000

    Adding retrofit

--- app/src/main/java/Tab/Tab1.java
+++ app/src/main/java/Tab/Tab1.java
@@ -3,6 +3,7 @@ package Tab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +12,16 @@ import gdg.spanish.themoviedb.R;
 
 public class Tab1 extends Fragment {
 
+    private RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.tab_1, null);
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view_tab, container, false);
 
         //TODO: Implement content
 
-        return v;
+        return recyclerView;
     }
 
 }
